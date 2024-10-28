4f9e47153847e21991ab6e4d13fecbbb3ae48c6d
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-10-28 22:40:10 +0300

    Refactor payment component to use theme from next-themes

--- components/cart/payment.tsx
+++ components/cart/payment.tsx
@@ -4,17 +4,24 @@ import getStripe from "@/lib/get-stripe";
 import { Elements } from "@stripe/react-stripe-js";
 import { motion } from "framer-motion";
 import PaymentForm from "./payment-form";
+import { useTheme } from "next-themes";
 
 export default function Payment() {
   const stripe = getStripe();
   const cart = useCartStore((state) => state.cart);
-  const total = cart.reduce((acc, item) => acc + item.price * item.variant.quantity, 0);
-  
+  const total = cart.reduce(
+    (acc, item) => acc + item.price * item.variant.quantity,
+    0
+  );
+  const {theme} = useTheme()
+
   return (
-    <motion.div>
+    <motion.div className="lg:px-20">
       <Elements
         stripe={stripe}
-        options={{ mode: "payment", currency: "usd", amount: total*100 }}
+        options={{ mode: "payment", currency: "usd", amount: total * 100 , appearance: {
+          theme: theme === "dark" ? "night" : "flat"
+        }}}
       >
         <PaymentForm total={total} />
       </Elements>
--- components/reviews/review.tsx
+++ components/reviews/review.tsx
@@ -9,29 +9,33 @@ import Stars from "./stars";
 export default function Review({ reviews }: { reviews: ReviewsWithUser[] }) {
   return (
     <motion.div className="flex flex-col gap-4 my-2">
-      {reviews.map((review) => (
-        <Card key={review.id} className="p-4">
-          <div className="flex gap-2 items-center">
-            <Image
-              className="rounded-full"
-              width={32}
-              height={32}
-              alt={review?.user?.name!}
-              src={review.user?.image!}
-            />
-            <div>
-              <p className="text-sm font-bold">{review?.user?.name}</p>
-              <div className="flex items-center gap-2">
-                <Stars rating={review.rating} />
-                <p className="text-xs font-bold text-muted-foreground">
-                  {formatDistance(subDays(review.created!, 0), new Date())}
-                </p>
+      {reviews.length === 0 && (
+        <p className="text-center text-muted-foreground py-2 font-medium">No reviews yet</p>
+      )}
+      {reviews.length > 0 &&
+        reviews.map((review) => (
+          <Card key={review.id} className="p-4">
+            <div className="flex gap-2 items-center">
+              <Image
+                className="rounded-full"
+                width={32}
+                height={32}
+                alt={review?.user?.name!}
+                src={review.user?.image!}
+              />
+              <div>
+                <p className="text-sm font-bold">{review?.user?.name}</p>
+                <div className="flex items-center gap-2">
+                  <Stars rating={review.rating} />
+                  <p className="text-xs font-bold text-muted-foreground">
+                    {formatDistance(subDays(review.created!, 0), new Date())}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <p className="py-2 font-medium">{review.comment}</p>
-        </Card>
-      ))}
+            <p className="py-2 font-medium">{review.comment}</p>
+          </Card>
+        ))}
     </motion.div>
   );
 }
