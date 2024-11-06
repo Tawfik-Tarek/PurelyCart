397b3c9a79f640ea69034ee1d151bc5e396b5370
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-11-06 17:25:23 +0200

    Add weekly earnings chart and check date utility

--- app/dashboard/analytics/earnings.tsx
+++ app/dashboard/analytics/earnings.tsx
@@ -6,6 +6,9 @@ import FormatPrice from "@/lib/format-price";
 import { TotalOrders } from "@/lib/infer-type";
 import { cn } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import WeeklyChart from "./weekly-chart";
+import { useMemo } from "react";
 
 export default function Earnings({ totalOrders }: { totalOrders: TotalOrders[] }) {
   const router = useRouter();
@@ -15,6 +18,18 @@ export default function Earnings({ totalOrders }: { totalOrders: TotalOrders[] }
   const totalRevenue = totalOrders.reduce((sum, order) => sum + order.product.price * order.quantity, 0);
   const totalOrdersCount = totalOrders.length;
   const totalProducts = totalOrders.reduce((sum, order) => sum + order.quantity, 0);
+
+  const chartItems = totalOrders.map((order) => ({
+    date: order.order.created!,
+    revenue: order.order.total,
+  }))
+
+  const activeChart = useMemo(() => {
+    const weekly = WeeklyChart(chartItems)
+    if (period === "week") {
+      return weekly
+    }
+  }, [period])
 
   const handleRouteChange = (url: string) => {
     router.push(url, { scroll: false });
@@ -42,15 +57,33 @@ export default function Earnings({ totalOrders }: { totalOrders: TotalOrders[] }
           This Month
         </Badge>
       </div>
-      <CardContent>
+      <CardContent className="h-96">
         {totalOrdersCount === 0 ? (
           <div>You have no earnings</div>
         ) : (
-          <div>
-            <div>Revenue: {formattedTotalRevenue}</div>
-            <div>Orders: {totalOrdersCount}</div>
-            <div>Products: {totalProducts}</div>
-          </div>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart data={activeChart}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip content={(props) => {
+                return (
+                  <div>
+                    {props.payload?.map((item, index) => {
+                      return (
+                        <div key={item.payload.date} className="bg-white text-primary p-2 rounded-md shadow-md">
+                          <p>Date: {item.payload.date}</p>
+                          <p>Revenue: {item.payload.revenue}$</p>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
+              }} />
+              <Legend />
+              <YAxis dataKey="revenue" />
+              <XAxis dataKey="date" />
+              <Bar dataKey="revenue" className="fill-primary" />
+            </BarChart>
+          </ResponsiveContainer>
         )}
       </CardContent>
     </Card>
