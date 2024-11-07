7011289108b05e9fe92d708ac27aec54ac2e0fab
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-11-07 09:03:23 +0200

    Filter products by tag using search parameters

--- components/products/products.tsx
+++ components/products/products.tsx
@@ -5,15 +5,29 @@ import Image from "next/image";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
 import FormatPrice from "@/lib/format-price";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 
 type ProductTypes = {
   variants: VariantsWithProduct[];
 };
 
 export function Products({ variants }: ProductTypes) {
+  const params = useSearchParams()
+  const paramTag = params.get("tag")
+
+  const filtered = useMemo(() => {
+    if (paramTag && variants) {
+      return variants.filter((variant) =>
+        variant.variantTags.some((tag) => tag.tag === paramTag)
+      )
+    }
+    return variants
+  }, [paramTag])
+
   return (
     <main className="grid grid-cols-1 gap-4 py-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-10  place-items-end ">
-      {variants.map((variant) => (
+      {filtered.map((variant) => (
         <Link
           key={variant.id}
           href={`/products/${variant.id}?id=${variant.id}&productId=${variant.productId}&price=${variant.product.price}&title=${variant.product.title}&type=${variant.productType}&image=${variant.variantImages[0].url}`}
